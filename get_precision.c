eac7b3dcff25e423d943f4630a5faf1dcaa6c55e
peniel124166 <113617200+peniel124166@users.noreply.github.com>
2022-11-22 16:31:56 -0800

    team projects

--- get_precision.c
+++ get_precision.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * get_precision - Calculates the precision for printing
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
+ * get_precision - the precision for printing.
+ * @format: Formatted string in which to print the arguments.
+ * @i: List of arguments.
  * @list: list of arguments.
  *
  * Return: Precision.
